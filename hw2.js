625e2c3b60197dc26a880b3aad46a396746952b2
roush55 <ranaabumelha@gmail.com>
2019-11-19 10:35:58 +0300

    final update

---
+++ hw2.js
@@ -0,0 +1,69 @@
+/* Your solution here:
+1. What is `a` ?
+    
+`
+a is ...
+'bongos'
+
+2. What is `b` ?
+`
+b is true 
+true 
+3. What is `c` ?
+
+c is ...
+`'bongos'
+*/
+
+const firstWord = "Please";
+const secondWord = "squeeze";
+const thirdWord = "the";
+const thirdWord = "cheese";
+console.log(firstWord+secondWord+thirdWord+thirdWord)
+const num1 = 5;
+const num2 = 10;
+let num3=num1+num2 ;
+console.log('the sum of ' +num1+' and '+num2+' is '+num3);
+/*a)false
+b) true 
+c) false
+d) false
+e) false
+f) false
+g) true
+h) false */
+
+/** 
+ let i=100;
+  if (i%3 ==0 && i%5==0){
+console.log("FizzBuzz")
+else if (i%3==0){
+    console.log("Fizz")}
+else if (i%5==0){
+    console.log("buzz")}
+else{
+    console.log(i)
+}
+} 
+/**let result;
+ const x=20;
+ function testNum(){
+ for (let i=1:i<=x:i+=1):
+ if i%3 ==0 && i%5==0
+ result="FizzBuzz";
+
+ else if (i%3==0)
+  result="Fizz";
+else if (i%5==0){
+    
+    result="Buzz";
+else{
+    result=i;
+ }
+ console.log(result);
+}
+
+ */
+
+
+
